eb5a30bca09c6ba9d274b0b096195935a44d05fd
blair.james@comparethemarket.com.au <blair@blairjames.com>
2024-07-11 05:50:34 +1000

    Add docstrings to tfn_validator.py

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 main() {
   sudo docker build -t docker.io/blairy/tfn-validator . || exit 1
+  sudo docker push docker.io/blairy/tfn-validator || exit 1
+  curl -X POST https://hub.docker.com/api/build/v1/source/222a93ce-2bc4-425b-9c45-473f589d8e22/trigger/760b3256-9bde-4821-96ff-3e1daa09309a/call/ || exit 1
 }
 
 main
--- tfn_validator.py
+++ tfn_validator.py
@@ -7,34 +7,52 @@ class TfnValidator:
     """
 
     def __init__(self, candidate: list) -> None:
+        """
+        Instance variables
+        """
         self.candidate: list = candidate
         self.weighting: list = [1, 4, 3, 7, 5, 8, 6, 9, 10]
         self.total: int = 0
 
     def validate(self):
-        i = 0
-        while i < len(self.candidate):
-            result: int = int(self.candidate[i]) * int(self.weighting[i])
-            self.total = self.total + result
-            i += 1
+        """
+        Multiply each digit of the candidate TFN by the corresponding weighting
+        and sum them together.
+        """
+        try:
+            i = 0
+            while i < len(self.candidate):
+                result: int = int(self.candidate[i]) * int(self.weighting[i])
+                self.total = self.total + result
+                i += 1
+        except Exception as e:
+            raise SystemExit(e) from e
 
     def checkmod(self):
-        mod = self.total % 11
-        if mod == 0:
-            print("".join(self.candidate) + " is a valid TFN.")
-        else:
-            print("".join(self.candidate) + " is NOT a valid TFN.")
+        """
+        If the sum total of all digits multiplied by the weighting
+        is divisible by 11, the candidate is a valid TFN.
+        """
+        try:            
+            mod = self.total % 11
+            if mod == 0:
+                print("".join(self.candidate) + " is a valid TFN.")
+            else:
+                print("".join(self.candidate) + " is NOT a valid TFN.")
+        except Exception as e:
+            raise SystemExit(e) from e
 
 
 def main():
+    """
+    main 
+    """
     try:
-
         def get_candidate():
             candidate: list = list(input("Enter potential TFN: ").replace(" ", ""))
             return candidate
-
     except Exception as e:
-        raise SystemError(e)
+        raise SystemError(e) from e
     tfn = TfnValidator(get_candidate())
     tfn.validate()
     tfn.checkmod()
